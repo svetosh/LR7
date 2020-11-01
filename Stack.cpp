2ba149560d03d82d56ee46dc21b9d0f2947bce08
svetosh <70635136+svetosh@users.noreply.github.com>
2020-11-01 17:15:18 +0300

    Add files via upload

---
+++ Stack.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+struct Node
+{
+    Node* nextNode;
+    int value;
+};
+
+struct Stack
+{
+    Node* endNode;
+    int sizeStack;
+};
+
+void constructor(Stack& stack)
+{
+    stack.endNode = nullptr;
+    stack.sizeStack = 0;
+}
+
+int pop(Stack& stack)
+{
+    int value = stack.endNode->value;
+    Node* popNode = stack.endNode;
+    stack.endNode = popNode->nextNode;
+    delete popNode;
+    --stack.sizeStack;
+    return value;
+}
+
+void push(Stack& stack, int value)
+{
+    Node* pushNode;
+    Node* endAdd = stack.endNode;
+    pushNode = new Node{ endAdd, value };
+    stack.endNode = pushNode;
+    ++stack.sizeStack;
+}
+
+int new_size(Stack& stack)
+{
+    return stack.sizeStack;
+}
+
+void destructor(Stack& stack)
+{
+    while (stack.endNode != nullptr)
+    {
+        std::cout << pop(stack) << " ";
+    } 
+}
+
+int main()
+{
+    Stack Jopa;
+    constructor(Jopa);
+    for (int i = 0; i < 10; i++)
+    {
+        push(Jopa, i);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        pop(Jopa);
+    }
+    destructor(Jopa);
+}
