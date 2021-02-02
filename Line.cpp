2ec26275b9f9d8a9b99c7067298d6399764b0595
svetosh <70635136+svetosh@users.noreply.github.com>
2021-02-02 19:22:10 +0300

    Update Line.cpp

--- Line.cpp
+++ Line.cpp
@@ -20,43 +20,42 @@ void constructor(Line& line)
     line.sizeLine = 0;
 }
 
+void push(Line& line, int value)
+{
+    Node* pushNode = new Node { nullptr, value };
+    if (line.endNode != nullptr)
+    {
+        line.endNode->nextNode = pushNode;
+        line.endNode = pushNode;
+    }
+    else if (line.begNode == nullptr)
+    {
+        line.begNode = pushNode;
+        line.endNode = pushNode;
+    }
+    ++line.sizeLine;
+}
+
 int pop(Line& line)
 {
-    if (line.lineSize == 0)
+    if (line.sizeLine == 0)
     {
         return 0;
     }
     else
     {
-    int value = line.begNode->value;
-    Node* popNode = line.begNode;
-    line.begNode = popNode->nextNode;
-    delete popNode;
-    --line.sizeLine;
-    return value;
+        int value = line.begNode->value;
+        Node* popNode = line.begNode;
+        line.begNode = popNode->nextNode;
+        delete popNode;
+        --line.sizeLine;
+        return value;
     }
 }
 
 int size(Line& line)
 {
-    return line.lineSize;
-}
-
-void push(Line& line, int value)
-{
-    Node* pushNode;
-    
-    pushNode = new Node{ nullptr, value };
-    if (line.endNode != nullptr)
-    {
-        line.endNode->nextNode = pushNode;
-    }
-    line.endNode = pushNode;
-    if (line.begNode == nullptr)
-    {
-        line.begNode = line.endNode;
-    }
-    ++line.sizeLine;
+    return line.sizeLine;
 }
 
 void destructor(Line& line)
@@ -71,7 +70,7 @@ int main()
 {
     Line Joja;
     constructor(Joja);
-    for (int i = 0; i < 10; i++)
+    for (int i = 1; i < 11; i++)
     {
         push(Joja, i);
     }
